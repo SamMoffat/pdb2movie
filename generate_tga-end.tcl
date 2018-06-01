794b85c02fa5af93ced17283f5ee57040f56c7e7
Sam Moffat <sam.moffat@hotmail.com>
2018-06-01 15:24:41 +0100

    Re-enabled object rendering in .tcl scripts.

--- generate_tga-end.tcl
+++ generate_tga-end.tcl
@@ -1,13 +1,13 @@
 # now writing out the images/objects
 for {set framecount 0} {$framecount < $frame} {incr framecount 1} {
     set imgfilename [format $outputimgfolder $framecount]
-    #set objfilename [format $outputobjfolder $framecount]
+    set objfilename [format $outputobjfolder $framecount]
 
     # images
     render snapshot $imgfilename
     # objects
     animate goto $framecount
-    #render Wavefront $objfilename
+    render Wavefront $objfilename
 }
 
 quit
--- generate_tga-start.tcl
+++ generate_tga-start.tcl
@@ -2,7 +2,7 @@ set frame  0
 axes location off
 set folder [lindex $argv 0]
 set output ${folder}/outputs
-#set outputobjfolder ${output}/animate.1%07d.obj
+set outputobjfolder ${output}/animate.1%07d.obj
 set outputimgfolder ${output}/image.1%07d.tga
 
 foreach pdb [lsort -decreasing [glob ${folder}/tmp_froda*.pdb]] { 
